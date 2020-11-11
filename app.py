641510c1ca20365642a4ef3e3bb057b656d726f1
nootify <48371116+nootify@users.noreply.github.com>
2020-11-11 16:51:57 -0500

    Add good and bad input form

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import render_template
+from flask import render_template, request
 
 from server import create_app, run_app
 
@@ -15,6 +15,18 @@ app = create_app(static_folder=STATIC_FOLDER, template_folder=TEMPLATE_FOLDER)
 def index():
     now = datetime.now().strftime("%Y/%-m/%-d %-I:%M:%S %p")
     return render_template("index.html", time=now)
+
+
+@app.route("/bad", methods=["POST"])
+def vulnerable():
+    data = request.form["bad-input"]
+    return render_template("bad.html", user_input=data)
+
+
+@app.route("/good", methods=["POST"])
+def ok():
+    data = request.form["good-input"]
+    return render_template("good.html", user_input=data)
 
 
 if __name__ == "__main__":
