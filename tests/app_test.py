244011ff78c279d316fb577c6a80a358588d38ab
nootify <48371116+nootify@users.noreply.github.com>
2020-11-12 16:52:02 -0500

    Auto format python files; style main html page

--- tests/app_test.py
+++ tests/app_test.py
@@ -5,7 +5,7 @@ from app import app
 
 @pytest.fixture()
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
 
     with app.test_client() as client:
         yield client
@@ -18,31 +18,31 @@ def mock_input():
 
 class TestPages:
     def test_main_page(self, client):
-        page = client.get('/')
-        assert b'CS351 Demo' in page.data
+        page = client.get("/")
+        assert b"Hello World" in page.data
 
     def test_bad_page(self, client):
-        page = client.get('/bad')
-        assert b'Direct access not allowed.' in page.data
-    
+        page = client.get("/bad")
+        assert b"Direct access not allowed." in page.data
+
     def test_good_page(self, client):
-        page = client.get('/good')
-        assert b'Direct access not allowed.' in page.data
-    
+        page = client.get("/good")
+        assert b"Direct access not allowed." in page.data
+
     def test_fixme_page(self, client):
-        page = client.get('/fixme')
-        assert b'Please fix me.' in page.data
+        page = client.get("/fixme")
+        assert b"Please fix me." in page.data
 
 
 class TestForms:
     def test_bad_form(self, client, mock_input):
-        page = client.post('/bad', data={"bad-input": mock_input})
-        assert mock_input.encode('ascii') in page.data
+        page = client.post("/bad", data={"bad-input": mock_input})
+        assert mock_input.encode("ascii") in page.data
 
     def test_good_form(self, client, mock_input):
-        page = client.post('/good', data={"good-input": mock_input})
-        assert mock_input.encode('ascii') in page.data
-    
+        page = client.post("/good", data={"good-input": mock_input})
+        assert mock_input.encode("ascii") in page.data
+
     def test_fixme_form(self, client, mock_input):
-        page = client.post('/fixme', data={"fixme-input": mock_input})
-        assert mock_input.encode('ascii') in page.data
+        page = client.post("/fixme", data={"fixme-input": mock_input})
+        assert mock_input.encode("ascii") in page.data
